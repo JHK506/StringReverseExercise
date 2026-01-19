0ff027e67dd08ff4e0f81d9b44fa1bf2717bb8d5
Jesse <jesse.hek@outlook.com>
2026-01-19 20:33:48 +0100

    alles klaar

--- tests/StringReverseExercise.Tests/ExerciseTests.cs
+++ tests/StringReverseExercise.Tests/ExerciseTests.cs
@@ -16,6 +16,8 @@ public class ExerciseTests
 	[InlineData("Hello developer. Do you like any of these fruits: apple, pear, banana? I do!", "do! I banana? pear, apple, fruits: these of any like you Do developer. Hello")]
 	[InlineData("Hello developer. I like an apple. Do you like an apple too? Do you like an apple too?", "too? apple an like you Do too? apple an like you Do apple. an like I developer. Hello")]
 	[InlineData("Hello World!", "World! Hello")]
+
+
 	public void Opdracht1_WithText_ReturnsReversedText(string s, string expected)
 	{
 		//arrange
@@ -32,13 +34,14 @@ public class ExerciseTests
 	[InlineData("Hello developer. Do you like any of these fruits: apple, pear, banana? I do!", "olleH .repoleved oD uoy ekil yna fo eseht :stiurf ,elppa ,raep ?ananab I !od")]
 	[InlineData("Hello developer. I like an apple. Do you like an apple too? Do you like an apple too?", "olleH .repoleved I ekil na .elppa oD uoy ekil na elppa ?oot oD uoy ekil na elppa ?oot")]
 	[InlineData("Hello World!", "olleH !dlroW")]
-	public void Opdracht2_WithText_ReturnsReversedWords(string s, string expected)
+	
+	public void Opdracht2_WithText_ReturnsReversedWords(string s2, string expected)
 	{
 		//arrange
 		var c = new StringReverser();
 
 		//act
-		string result = c.Exercise2(s);
+		string? result = c.Exercise2(s2);
 
 		//assert
 		Assert.Equal(expected, result);
@@ -48,13 +51,13 @@ public class ExerciseTests
 	[InlineData("Hello developer. Do you like any of these fruits: apple, pear, banana? I do!", "OLLEh .REPOLEVED Od UOY EKIL YNA FO ESEHT :STIURF ,ELPPA ,RAEP ?ANANAB i !OD")]
 	[InlineData("Hello developer. I like an apple. Do you like an apple too? Do you like an apple too?", "OLLEh .REPOLEVED i EKIL NA .ELPPA Od UOY EKIL NA ELPPA ?OOT Od UOY EKIL NA ELPPA ?OOT")]
 	[InlineData("Hello World!", "OLLEh !DLROw")]
-	public void Opdracht3_1_WithText_ReturnsReversedCasing(string s, string expected)
+	public void Opdracht3_1_WithText_ReturnsReversedCasing(string s31, string expected)
 	{
 		//arrange
 		var c = new StringReverser();
 
 		//act
-		string result = c.Exercise31(s);
+		string? result = c.Exercise31(s31);
 
 		//assert
 		Assert.Equal(expected, result);
@@ -64,13 +67,13 @@ public class ExerciseTests
 	[InlineData("Hello developer. Do you like any of these fruits: apple, pear, banana? I do!", "Olleh .repoleved Od Uoy Ekil Yna Fo Eseht :stiurf ,elppa ,raep ?ananab I !od")]
 	[InlineData("Hello developer. I like an apple. Do you like an apple too? Do you like an apple too?", "Olleh .repoleved I Ekil Na .elppa Od Uoy Ekil Na Elppa ?oot Od Uoy Ekil Na Elppa ?oot")]
 	[InlineData("Hello World!", "Olleh !dlrow")]
-	public void Opdracht3_2_WithText_ReturnsReversedEachWordCapitalized(string s, string expected)
+	public void Opdracht3_2_WithText_ReturnsReversedEachWordCapitalized(string s32, string expected)
 	{
 		//arrange
 		var c = new StringReverser();
 
 		//act
-		string result = c.Exercise32(s);
+		string? result = c.Exercise32(s32);
 
 		//assert
 		Assert.Equal(expected, result);
@@ -80,13 +83,13 @@ public class ExerciseTests
 	[InlineData("Hello developer. Do you like any of these fruits: apple, pear, banana? I do!", "Olleh .Repoleved od uoy ekil yna fo eseht :stiurf ,elppa ,raep ?Ananab i !Od")]
 	[InlineData("Hello developer. I like an apple. Do you like an apple too? Do you like an apple too?", "Olleh .Repoleved i ekil na .Elppa od uoy ekil na elppa ?Oot od uoy ekil na elppa ?Oot")]
 	[InlineData("Hello World!", "Olleh !Dlrow")]
-	public void Opdracht3_3_WithText_ReturnsReversedEachFirstWordCapitalized(string s, string expected)
+	public void Opdracht3_3_WithText_ReturnsReversedEachFirstWordCapitalized(string s33, string expected)
 	{
 		//arrange
 		var c = new StringReverser();
 
 		//act
-		string result = c.Exercise33(s);
+		string? result = c.Exercise33(s33);
 
 		//assert
 		Assert.Equal(expected, result);
---
+++ tests/StringReverseExercise.Tests/StringReverser.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace StringReverseExercise.Tests
+{
+    public class StringReverser
+    {
+        public string Exercise1(string s)
+        {
+            var words = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Array.Reverse(words);
+            return string.Join(" ", words);
+        }
+        public string Exercise2(string s2)
+        {
+            var words = s2.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                char[] chars = words[i].ToCharArray();
+                Array.Reverse(chars);
+                words[i] = new string(chars);
+            }
+            return string.Join(" ", words);
+        }
+        public string Exercise31(string s31) 
+        {
+            var words = s31.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                char[] chars = words[i].ToCharArray();
+                Array.Reverse(chars);
+                for (int j = 0; j < chars.Length; j++)
+                    chars[j] = char.IsUpper(chars[j]) ? char.ToLower(chars[j]) : char.ToUpper(chars[j]);
+                
+                    words[i] = new string(chars);
+            }
+            return string.Join(" ", words);
+        }
+        public string Exercise32(string s32)
+        {
+            var words = s32.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                char[] chars = words[i].ToCharArray();
+                Array.Reverse(chars);
+                words[i] = new string(chars);
+            }
+            for (int j = 0; j < words.Length; j++)            
+                if (!string.IsNullOrEmpty(words[j]))
+                    words[j] = char.ToUpper(words[j][0]) + words[j].Substring(1).ToLower();                
+            
+            return string.Join(" ", words);
+        }
+        public string Exercise33(string s33)
+        {
+            bool upperCase = true;
+            var words = s33.Split(' ', StringSplitOptions.None);
+            for (int i = 0; i < words.Length; i++)
+                words[i] = new string(words[i].Reverse().ToArray());
+            
+            var chars = string.Join(" ", words).ToLower().ToCharArray();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (upperCase && char.IsLetter(chars[i]))                
+                    chars[i] = char.ToUpper(chars[i]);
+                    upperCase = false;
+                
+                if (chars[i] is '.' or '!' or '?')
+                    upperCase = true;
+            }
+            return new string(chars);
+        }
+    }
+}
